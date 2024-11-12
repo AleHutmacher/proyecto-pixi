d0fd5b4e1d2a612606ffdf49693e912f503c2424
AleHutmacher <hutmacheralejo@gmail.com>
2024-11-12 19:22:08 -0300

    add(global): pixi installed

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import { Stage, Container, Sprite, Text } from "@pixi/react";
+
+function App() {
+  return (
+    <div className="stage-container">
+      <Stage className="stage" options={{ background: 0xffffff }}>
+        <Sprite
+          image="https://pixijs.io/pixi-react/img/bunny.png"
+          x={400}
+          y={270}
+          anchor={{ x: 0.5, y: 0.5 }}
+        />
+
+        <Container x={400} y={330}>
+          <Text text="Hello World" anchor={{ x: 0.5, y: 0.5 }} />
+        </Container>
+      </Stage>
+    </div>
+  );
+}
+
+export default App;
